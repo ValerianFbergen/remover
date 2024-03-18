fd825deda51918979f43103060442a3a44738f9d
Valerian Flauterspitczstrichtcinbergen <59750383+ValerianFbergen@users.noreply.github.com>
2024-03-18 12:59:06 +0300

    Delete removedot_.py

--- removedot_.py
+++
@@ -1,3 +0,0 @@
-from glob import glob;from os import getcwd,remove
-def incept(a):return sum([sorted(glob(i+'._*'))+incept(sorted(glob(i+'*\\')))for i in a],[])
-for i in incept([getcwd()]):print(i,remove(i))
